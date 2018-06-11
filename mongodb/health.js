86ba35876f22059856be9224d123444fcfac7cac
Gerardo Saca <gsacavdm@hotmail.com>
2018-06-10 23:48:11 -0700

    Health: switch to lambda, add healthMissing, add presentation instructions

--- mongodb/health.js
+++ mongodb/health.js
@@ -8,7 +8,7 @@ var h_mc = getRecord("health", 2).metadata.supportedResourceTypes;
 var h_bf = getRecord("health", 3).metadata.supportedResourceTypes;
 
 // Get delta for health
-h_delta = h_ww.map(function(h) { return { 
+h_delta = h_ww.map(h => { return { 
   namespace: h.match(rpRegEx)[0].toLowerCase(),
   resourceType: h.toLowerCase(),
   inFairfax: h_ff.indexOf(h) > -1 ? 1 : 0,
@@ -17,17 +17,24 @@ h_delta = h_ww.map(function(h) { return {
 }}).sort()
 dropAndInsert("healthDelta", h_delta)
 
+// Get count of missing health by sovereign
+var h_missing = {
+  Fairfax: h_delta.filter(h => h.inFairfax === 0 ).length,
+  Mooncake: h_delta.filter(h => h.inMooncake === 0 ).length,
+  Blackforest: h_delta.filter(h => h.inBlackforest === 0 ).length,
+}
+dropAndInsert("healthMissing", h_missing);
 
 // Get count of missing helath resource types by RP
 function getHealthMissingInSovereignBit(rpns, h_delta, sovereignBit) {
   return rpns[sovereignBit] === 0 ?
     "N/A" :
-    h_delta.filter(function(h) {
-      return h.namespace === rpns.namespace
+    h_delta.filter(h => 
+      h.namespace === rpns.namespace
       && h[sovereignBit] === 0
-    }).length
+    ).length
 }
-h_missing_by_ns = db.resourceProviderDelta.find().map(function(rpns) { return { 
+h_missing_by_ns = db.resourceProviderDelta.find().map(rpns => { return { 
   namespace: rpns.namespace,
   missingInFairfax: getHealthMissingInSovereignBit(rpns, h_delta, "inFairfax", false),
   missingInMooncake: getHealthMissingInSovereignBit(rpns, h_delta, "inMooncake", false),
@@ -38,8 +45,30 @@ dropAndInsert("healthMissingByNamespace", h_missing_by_ns)
 
 // Get count of resource providers missing one or more resource types in health by sovereign
 rpns_missing_h = {
-  Fairfax: h_missing_by_ns.filter(function(ns) { return ns.missingInFairfax > 0 }).length,
-  Mooncake: h_missing_by_ns.filter(function(ns) { return ns.missingInMooncake > 0 }).length,
-  Blackforest: h_missing_by_ns.filter(function(ns) { return ns.missingInBlackforest > 0 }).length
+  Fairfax: h_missing_by_ns.filter(ns => ns.missingInFairfax > 0 ).length,
+  Mooncake: h_missing_by_ns.filter(ns => ns.missingInMooncake > 0 ).length,
+  Blackforest: h_missing_by_ns.filter(ns => ns.missingInBlackforest > 0 ).length
 }
 dropAndInsert("resourceProviderMissingHealth", rpns_missing_h);
+
+
+/////////////////////////
+// Presenting this information
+/////////////////////////
+
+// These many "services" have missing "features" in Fairfax.
+// Considerations: 
+//  * RP used as proxy for service
+//  * Resource type onboarded to resource health used as proxy for feature
+//  * No notion of preview for health
+db.resourceProviderMissingHealth.find()
+
+// Get all the RPs with missing roles in Fairfax
+db.healthMissingByNamespace.find({ missingInFairfax : { $gt : 0 } }, { _id : 0, namespace : 1 , missingInFairfax : 1 })
+
+// Get all the resource types missing resource health onboarding in Fairfax
+// ACTION: Get status/reason for each one.
+db.healthDelta.find({ namespace: "microsoft.apimanagement", inFairfax : 0 }, { _id: 0 })
+
+// ACTION: Get the RBAC guys to introduce VERSIONS
+// ACTION: Get the RBAC guys to standardize approach for preview
