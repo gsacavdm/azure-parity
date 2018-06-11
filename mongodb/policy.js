c67741938c9d94a5ab38a135da669ba077f7cb80
Gerardo Saca <gsacavdm@hotmail.com>
2018-06-10 23:51:02 -0700

    Policy: switch to lambda, different detection for preview, add presentation instructions

--- mongodb/policy.js
+++ mongodb/policy.js
@@ -8,19 +8,19 @@ function getPolicyDefinition(arr, i) {
     .skip(i)
     .limit(1)[0]
     .value
-    .map(function(p)
+    .map(p =>
       { return {
         name: p.name,
         displayName: p.properties.displayName,
-        preview: safeGet(p, "properties.metadata.preview", false),
+        preview: p.properties.displayName.indexOf("[Preview]") > -1,
         deprecated: safeGet(p, "properties.metadata.deprecated", false),
         resourceProviders: function (properties) {
           var rps = JSON.stringify(properties).match(rpRegEx);
           if (rps) {
-            rps = rps.map(function (rp) { return rp.toLowerCase() });
+            rps = rps.map(rp => rp.toLowerCase());
 
             // Get distinct list of RPs
-            return rps.filter(function(rp, i) { return rps.indexOf(rp) === i; })
+            return rps.filter((rp, i) => rps.indexOf(rp) === i)
           } else {
             return [];
           }
@@ -34,26 +34,26 @@ var p_mc = getPolicyDefinition(db.policy,2);
 var p_bf = getPolicyDefinition(db.policy,3);
 
 // Get delta for policies
-var p_delta = p_ww.map(function(p) { return { 
+var p_delta = p_ww.map(p => { return { 
   name: p.name, 
   displayName: p.displayName, 
   preview: p.preview, 
   deprecated: p.deprecated,
   resourceProviders: p.resourceProviders,
-  inFairfax: p_ff.filter(function(ff) { return ff.name === p.name }).length, 
-  inMooncake: p_mc.filter(function(mc) { return mc.name === p.name }).length, 
-  inBlackforest: p_bf.filter(function(bf) { return bf.name === p.name }).length 
+  inFairfax: p_ff.filter(ff => ff.name === p.name ).length, 
+  inMooncake: p_mc.filter(mc => mc.name === p.name ).length, 
+  inBlackforest: p_bf.filter(bf => bf.name === p.name ).length 
 }});
 dropAndInsert("policyDelta", p_delta);
 
 // Get count of missing policies by sovereign
 var p_missing = {
-  Fairfax: p_delta.filter(function(p) { return !p.deprecated && !p.preview && p.inFairfax === 0 }).length,
-  Mooncake: p_delta.filter(function(p) { return !p.deprecated && !p.preview && p.inMooncake === 0 }).length,
-  Blackforest: p_delta.filter(function(p) { return !p.deprecated && !p.preview && p.inBlackforest === 0 }).length,
-  FairfaxIncludingPreview: p_delta.filter(function(p) { return !p.deprecated && p.inFairfax === 0 }).length,
-  MooncakeIncludingPreview: p_delta.filter(function(p) { return !p.deprecated && p.inMooncake === 0 }).length,
-  BlackforestIncludingPreview: p_delta.filter(function(p) { return !p.deprecated && p.inBlackforest === 0 }).length
+  Fairfax: p_delta.filter(p => !p.deprecated && !p.preview && p.inFairfax === 0 ).length,
+  Mooncake: p_delta.filter(p => !p.deprecated && !p.preview && p.inMooncake === 0 ).length,
+  Blackforest: p_delta.filter(p => !p.deprecated && !p.preview && p.inBlackforest === 0 ).length,
+  FairfaxIncludingPreview: p_delta.filter(p => !p.deprecated && p.inFairfax === 0 ).length,
+  MooncakeIncludingPreview: p_delta.filter(p => !p.deprecated && p.inMooncake === 0 ).length,
+  BlackforestIncludingPreview: p_delta.filter(p => !p.deprecated && p.inBlackforest === 0 ).length
 }
 dropAndInsert("policyMissing", p_missing);
 
@@ -61,14 +61,14 @@ dropAndInsert("policyMissing", p_missing);
 function getPolicyMissingInSovereignBit(rpns, p_delta, sovereignBit, includePreview) {
   return rpns[sovereignBit] === 0 ?
     "N/A" :
-    p_delta.filter(function(p) {
-      return p.resourceProviders.indexOf(rpns.namespace) > -1
+    p_delta.filter(p => 
+      p.resourceProviders.indexOf(rpns.namespace) > -1
       && p[sovereignBit] === 0
       && (includePreview || !p.preview)
       && !p.deprecated
-    }).length
+    ).length
 }
-p_missing_by_ns = db.resourceProviderDelta.find().map(function(rpns) { return { 
+p_missing_by_ns = db.resourceProviderDelta.find().map(rpns => { return { 
   namespace: rpns.namespace,
   missingInFairfax: getPolicyMissingInSovereignBit(rpns, p_delta, "inFairfax", false),
   missingInMooncake: getPolicyMissingInSovereignBit(rpns, p_delta, "inMooncake", false),
@@ -81,15 +81,33 @@ dropAndInsert("policyMissingByNamespace", p_missing_by_ns)
 
 // Get count of resource providers missing one or more policies by sovereign
 rpns_missing_p = {
-  Fairfax: p_missing_by_ns.filter(function(ns) { return ns.missingInFairfax > 0 }).length,
-  Mooncake: p_missing_by_ns.filter(function(ns) { return ns.missingInMooncake > 0 }).length,
-  Blackforest: p_missing_by_ns.filter(function(ns) { return ns.missingInBlackforest > 0 }).length,
-  FairfaxIncludingPreview: p_missing_by_ns.filter(function(ns) { return ns.missingInFairfaxIncludingPreview > 0 }).length,
-  MooncakeIncludingPreview: p_missing_by_ns.filter(function(ns) { return ns.missingInMooncakeIncludingPreview > 0 }).length,
-  BlackforestIncludingPreview: p_missing_by_ns.filter(function(ns) { return ns.missingInBlackforestIncludingPreview > 0 }).length
+  Fairfax: p_missing_by_ns.filter(ns => ns.missingInFairfax > 0 ).length,
+  Mooncake: p_missing_by_ns.filter(ns => ns.missingInMooncake > 0 ).length,
+  Blackforest: p_missing_by_ns.filter(ns => ns.missingInBlackforest > 0 ).length,
+  FairfaxIncludingPreview: p_missing_by_ns.filter(ns => ns.missingInFairfaxIncludingPreview > 0 ).length,
+  MooncakeIncludingPreview: p_missing_by_ns.filter(ns => ns.missingInMooncakeIncludingPreview > 0 ).length,
+  BlackforestIncludingPreview: p_missing_by_ns.filter(ns => ns.missingInBlackforestIncludingPreview > 0 ).length
 }
 dropAndInsert("resourceProviderMissingPolicy", rpns_missing_p);
 
 
+/////////////////////////
+// Presenting this information
+/////////////////////////
+
+// These many "services" have missing "features" in Fairfax.
+// Considerations: 
+//  * RP used as proxy for service
+//  * Policy used as proxy for feature
+//  * Preview in policy name used as a proxy for preview features
+db.resourceProviderMissingPolicy.find()
+
+// Get all the RPs with missing policies in Fairfax
+db.policyMissingByNamespace.find({ missingInFairfax : { $gt : 0 } }, { _id : 0, namespace : 1 , missingInFairfax : 1 })
+
+// Get all the missing policies in Fairfax
+// ACTION: Get status/reason for each one.
+db.policyDelta.find({ resourceProviders: "microsoft.sql", inFairfax : 0 }, { _id: 0 })
 
 // ACTION: Get the policy guys to introduce VERSIONS
+// ACTION: Get the policy guys to standardize approach for preview
