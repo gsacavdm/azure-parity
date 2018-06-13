34db49620124be42dc288874301ad48f7fe283a8
Gerardo Saca <gsacavdm@hotmail.com>
2018-06-12 18:11:59 -0700

    Fix feature.js (didn't work)

--- mongodb/feature.js
+++ mongodb/feature.js
@@ -18,27 +18,27 @@ function getMissingFeatureCount(rp, sovereign) {
     + rp.missingRole["missingIn" + sovereign]
     + rp.missingHealth["missingIn" + sovereign]
 }
-rp_o.filter(rp => 
+f_missing_ns.filter(rp => 
   rp.inFairfax === 1 || rp.inMooncake === 1 || rp.inBlackforest == 1
 ).forEach(rp => {
   getMissingFeatureCount(rp, "Fairfax");
   getMissingFeatureCount(rp, "Mooncake");
   getMissingFeatureCount(rp, "Blackforest");
 })
-dropAndInsert("featureMissingByNamespace", rp_o);
+dropAndInsert("featureMissingByNamespace", f_missing_ns);
 
 // Get count of missing features by sovereign
 f_missing = {
-  Fairfax: db.featuresMissingByNamespace.find({inFairfax:1}).map(ns => ns.missingFeaturesInFairfax).reduce((prev, next) => prev+next),
-  Mooncake: db.featuresMissingByNamespace.find({inMooncake:1}).map(ns => ns.missingFeaturesInMooncake).reduce((prev, next) => prev+next),
-  Blackforest: db.featuresMissingByNamespace.find({inBlackforest:1}).map(ns => ns.missingFeaturesInBlackforest).reduce((prev, next) => prev+next)
+  Fairfax: db.featureMissingByNamespace.find({inFairfax:1}).map(ns => ns.missingFeaturesInFairfax).reduce((prev, next) => prev+next),
+  Mooncake: db.featureMissingByNamespace.find({inMooncake:1}).map(ns => ns.missingFeaturesInMooncake).reduce((prev, next) => prev+next),
+  Blackforest: db.featureMissingByNamespace.find({inBlackforest:1}).map(ns => ns.missingFeaturesInBlackforest).reduce((prev, next) => prev+next)
 }
 dropAndInsert("featureMissing", f_missing);
 
 // Get count of resource providers missing one or more features by sovereign
 rpns_missing_f = {
-  Fairfax: db.featuresMissingByNamespace.find({inFairfax:1}).map(ns => ns.missingFeaturesInFairfax).filter(mf => mf > 0).length,
-  Mooncake: db.featuresMissingByNamespace.find({inMooncake:1}).map(ns => ns.missingFeaturesInMooncake).filter(mf => mf > 0).length,
-  Blackforest: db.featuresMissingByNamespace.find({inBlackforest:1}).map(ns => ns.missingFeaturesInBlackforest).filter(mf => mf > 0).length,
+  Fairfax: db.featureMissingByNamespace.find({inFairfax:1}).map(ns => ns.missingFeaturesInFairfax).filter(mf => mf > 0).length,
+  Mooncake: db.featureMissingByNamespace.find({inMooncake:1}).map(ns => ns.missingFeaturesInMooncake).filter(mf => mf > 0).length,
+  Blackforest: db.featureMissingByNamespace.find({inBlackforest:1}).map(ns => ns.missingFeaturesInBlackforest).filter(mf => mf > 0).length,
 }
 dropAndInsert("resourceProviderMissingFeature", rpns_missing_f);
