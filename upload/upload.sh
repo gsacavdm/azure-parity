406fb2035a311107dd12650c8974dc06e2134028
Gerardo Saca <gsacavdm@hotmail.com>
2018-06-18 09:51:43 -0700

    Use upload.sh as container entrypoint

--- upload/upload.sh
+++ upload/upload.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-DATABASE=$(date +%Y%m%d-%H%M%S)
-
-#while [ 1 ]
-#do
-echo "Uploading to $HOSTNAME in DB $DATABASE"
-find *.json -exec mongoimport --host $HOSTNAME -u $USERNAME -p $PASSWORD --ssl --sslAllowInvalidCertificates -d $DATABASE --file {} \;
-#  sleep 1d
-#done
+while [ 1 ]
+do
+  DATABASE=azure-parity-$(date +%Y%m%d)
+  echo "Uploading to $HOSTNAME in DB $DATABASE"
+  find *.json -exec mongoimport --host $HOSTNAME -u $USERNAME -p $PASSWORD --ssl --sslAllowInvalidCertificates -d $DATABASE --file {} \;
+  sleep 1d
+done
