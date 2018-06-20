6a7c7019763b556f09c8a48eb2db7fe039a6df7e
Gerardo Saca <gsacavdm@hotmail.com>
2018-06-20 03:27:28 -0400

    Factor out shared collection logic

--- collect-rps/Program.cs
+++ collect-rps/Program.cs
@@ -12,52 +12,16 @@ namespace azure_parity.collect_rps
     class Program
     {
         static bool Debug = true;
-        static int CycleTime = 24 * 60 * 60 * 1000; // 24 hours
 
         static void Main(string[] args)
         {
-            while (true) {
-                string configDirPath = utils.GetEnvironmentVariableOrFail("CONFIG_DIR_PATH");
-                string dataDirPath = utils.GetEnvironmentVariableOrFail("DATA_DIR_PATH");
-                var files = Directory.GetFiles(configDirPath);
-
-                foreach (var file in files) {
-                    Console.WriteLine("Processing " + file + "...");
-                    string cloudConfig = File.ReadAllText(file);
-
-                    Console.WriteLine("Getting cloud config...");
-                    JObject cloudConfigJson = JObject.Parse(cloudConfig);
-                    string cloudName = cloudConfigJson["CloudName"].Value<string>();
-                    string subscriptionId = cloudConfigJson["SubscriptionId"].Value<string>();
-                    string accessToken = cloudConfigJson["AccessToken"].Value<string>();
-                    string azureEndpoint = cloudConfigJson["AzureEndpoint"].Value<string>();
-
-                    Console.WriteLine("Getting resource providers...");
-                    string resourceProviderApiVersion = "2017-08-01";
-                    string resourceProviderEndpoint = 
-                        String.Format("{0}providers?api-version={1}", azureEndpoint, resourceProviderApiVersion);
-                    if (Debug) Console.WriteLine("ResourceProviderEndpoint: " + resourceProviderEndpoint);
-
-                    string resourceProvider = "{}";
-                    try {
-                        using (var httpClient = new HttpClient()) {
-                            httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
-                            resourceProvider = httpClient.GetStringAsync(resourceProviderEndpoint).Result;
-                        }
-                    } catch (Exception ex) {
-                        Console.WriteLine("WARN: Exception occurred. CloudName={0} EndPoint={1} ExceptionMessage={2}", 
-                            cloudName, resourceProviderEndpoint, ex.Message);
-                    }
-                    //Console.WriteLine(resourceProvider);
-                    
-                    var dataPath = String.Format("{0}/resourceProvider_{1}.json", dataDirPath, cloudName);
-                    Console.WriteLine(String.Format("Saving {0}", dataPath));
-                    File.WriteAllText(dataPath, resourceProvider);
-                }
-            
-                Console.WriteLine("Done!");
-                Task.Delay(CycleTime).Wait();
-            }
+            utils.Collect("resourceProvider", true, (subscriptionId, azureEndpoint, httpClient) => {
+                string resourceProviderApiVersion = "2017-08-01";
+                string resourceProviderEndpoint = 
+                    String.Format("{0}providers?api-version={1}", azureEndpoint, resourceProviderApiVersion);
+                if (Debug) Console.WriteLine("ResourceProviderEndpoint: " + resourceProviderEndpoint);
+                return httpClient.GetStringAsync(resourceProviderEndpoint).Result;
+            });
         }
     }
 }
--- shared/utils.cs
+++ shared/utils.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace azure_parity
 {
@@ -10,6 +16,57 @@ namespace azure_parity
                 throw new ArgumentException(String.Format("Environment Variable variable {0} has not been set.", name));
             }
             return value;
+        }
+
+        static int CycleTime = 24 * 60 * 60 * 1000; // 24 hours
+
+        public delegate string Collector(string subscriptionId, string endpoint, HttpClient httpClient);
+
+        public static void Collect(string sourceName, bool useArm, Collector CollectDetails)
+        {
+            while (true) {
+                string configDirPath = GetEnvironmentVariableOrFail("CONFIG_DIR_PATH");
+                string dataDirPath = GetEnvironmentVariableOrFail("DATA_DIR_PATH");
+                var files = Directory.GetFiles(configDirPath);
+
+                foreach (var file in files) {
+                    Console.WriteLine("Processing " + file + "...");
+                    string cloudConfig = File.ReadAllText(file);
+
+                    Console.WriteLine("Getting cloud config...");
+                    JObject cloudConfigJson = JObject.Parse(cloudConfig);
+                    string cloudName = cloudConfigJson["CloudName"].Value<string>();
+                    string subscriptionId = cloudConfigJson["SubscriptionId"].Value<string>();
+                    string accessToken = cloudConfigJson["AccessToken"].Value<string>();
+                    string azureEndpoint = cloudConfigJson["AzureEndpoint"].Value<string>();
+                    string portalEndpoint = cloudConfigJson["PortalEndpoint"].Value<string>();
+
+                    Console.WriteLine("Getting {0}...", sourceName);
+
+                    string data = "{}";
+                    try {
+                        using (var httpClient = new HttpClient()) {
+                            string endpoint = portalEndpoint;
+                            if (useArm) {
+                                httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+                                endpoint = azureEndpoint;
+                            }
+                            data = CollectDetails(subscriptionId, endpoint, httpClient);
+                        }
+                    } catch (Exception ex) {
+                        Console.WriteLine("WARN: Exception occurred. CloudName={0} SourceName={1} ExceptionMessage={2}", 
+                            cloudName, sourceName, ex.Message);
+                    }
+                    //Console.WriteLine(data);
+                    
+                    var dataPath = String.Format("{0}/{1}_{2}.json", dataDirPath, sourceName, cloudName);
+                    Console.WriteLine(String.Format("Saving {0}", dataPath));
+                    File.WriteAllText(dataPath, data);
+                }
+            
+                Console.WriteLine("Done!");
+                Task.Delay(CycleTime).Wait();
+            }
         }
     }
 }
