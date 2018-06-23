1bda6255c67b6b4c54e7f9994e5c18436263532c
Gerardo Saca <gsacavdm@hotmail.com>
2018-06-23 10:45:52 -0700

    ensure upload on first run

--- upload/upload.sh
+++ upload/upload.sh
@@ -5,7 +5,13 @@ while [ 1 ]
 do
   DATABASE=azure-parity-$(date +%Y%m%d)
   echo "Uploading to $HOSTNAME in DB $DATABASE"
-  find *.json -newer upload_checkpoint -exec mongoimport --host $HOSTNAME -u $USERNAME -p $PASSWORD --ssl --sslAllowInvalidCertificates -d $DATABASE --file {} \;
+  if [ ! -e upload_checkpoint ]; then
+    # if no checkpoint, process json files.
+    find *.json -exec mongoimport --host $HOSTNAME -u $USERNAME -p $PASSWORD --ssl --sslAllowInvalidCertificates -d $DATABASE --file {} \;
+  else
+    # if there is a checkpoint, get files created after that checkpoint.
+    find *.json -newer upload_checkpoint -exec mongoimport --host $HOSTNAME -u $USERNAME -p $PASSWORD --ssl --sslAllowInvalidCertificates -d $DATABASE --file {} \;
+  fi
   echo "" > upload_checkpoint
   sleep $SLEEP_DURATION
  done
